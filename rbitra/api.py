dbf221f7d489c1a7752073dda8f50f66d60260dc
Spencer Harmon <spencerharmon.91@gmail.com>
2018-04-22 13:01:35 -0500

    added framework for unit testing
    

---
+++ rbitra/api.py
@@ -0,0 +1,10 @@
+from app import app
+from flask_restful import Resource, Api
+
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
